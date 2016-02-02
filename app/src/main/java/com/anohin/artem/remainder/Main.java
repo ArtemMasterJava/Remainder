2acce487421a4a75b09de3aa066fac7b06a55e11
ArtemMasterJava <aanohin1@gmail.com>
2016-02-02 18:17:35 +0200

    add mainActivity and layout

--- app/src/main/java/com/anohin/artem/remainder/Main.java
+++
@@ -1,7 +0,0 @@
-package com.anohin.artem.remainder;
-
-/**
- * Created by Artem on 31.01.2016.
- */
-public class Main {
-}
---
+++ app/src/main/java/com/anohin/artem/remainder/MainActivity.java
@@ -0,0 +1,15 @@
+package com.anohin.artem.remainder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Artem on 31.01.2016.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
